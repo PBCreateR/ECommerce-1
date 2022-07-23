7be4cbe32cbc5d3591fc7fe00e714cd19b57ee64
PBCreateR <pb7038738722@gmail.com>
2022-07-23 15:26:56 +0530

    Pratik Modified for Authention

--- AuthenticationController.java
+++ AuthenticationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.pojos.AuthenticationResponse;
-
+//ABCD
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin//(origins = "http://localhost:4200")
